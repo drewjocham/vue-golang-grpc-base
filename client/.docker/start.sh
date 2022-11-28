f593a5074409624e6182694f4705555267bdc202
Drew Jocham <drew.jocham@gmail.com>
2022-11-28 20:50:26 +0100

    creating base infra with pulumi

--- client/.docker/start.sh
+++ client/.docker/start.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # Replace env vars in config template and save it as config file
-envsubst '${ODJ_SIBLING_BACKOFFICE_HOST},${API_PORT_SUFFIX},${ODJ_DEP_VARSGENERIC_ENV}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+#envsubst '${SIBLING_BACKOFFICE_HOST},${API_PORT_SUFFIX},${DEP_VARSGENERIC_ENV}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Run nginx
 exec /usr/sbin/nginx
