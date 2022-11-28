56a16529d8ace1684b7cf7f9f0005dd4a3cd7441
Drew Jocham <drew.jocham@gmail.com>
2022-11-28 23:35:41 +0100

    creating base infra with pulumi

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -12,7 +12,7 @@ export const api = {
 
     async getTest() {
         try{
-            return grpcClient.get<TestResponse>("http://localhost:8080/v1/test")
+            return await grpcClient.get<TestResponse>("http://localhost:8080/v1/test")
                 .then(res => {
                     return res.data
                 })
@@ -20,42 +20,6 @@ export const api = {
             console.log("error" + err);
         }
     },
-
-    /*
-    async getTest(): Promise<TestRequest> {
-        const req = new TestRequest();
-        req.setName("Mike");
-
-                   grpcClient.get<TestRequest>("http://localhost:8080/v1/test")
-                .then(async (res: AxiosResponse<TestResponse>) => {
-                    console.log("output:", res.data.getName())
-                    response.setName(res.data.getName())
-                })
-
-        return new Promise<TestRequest>((resolve, reject) => {
-            grpcClient.test(req, (err, res) => {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(res);
-            });
-        });
-    },
-
-    async testMethod() {
-        const req = new TestRequest();
-        req.setName("Drew");
-
-        grpcClient.test(req, (err, res) => {
-            if(!err) {
-                console.info("Greeting:", res.getName());
-            } else {
-                console.error("Error:", err.message);
-            }
-        })
-    }
-    */
-
 
 }
 
