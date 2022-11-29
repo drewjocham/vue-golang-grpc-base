95db8c50fcdf0f3af95e392048b66bf31d1ba4c6
Drew Jocham <drew.jocham@gmail.com>
2022-11-29 09:11:27 +0100

    created nginx config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 
-// vue.config.js
-module.exports = {
-    configureWebpack: config => {
-        if (process.env.NODE_ENV === 'production') {
-            // mutate config for production...
-        } else {
-            // mutate for development...
-        }
-    }
-}
+module.exports = defineConfig({
+
+})
+
