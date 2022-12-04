ad001cc4ec9c117112cb49317877cc486fffcd31
Drew Jocham <drew.jocham@gmail.com>
2022-12-04 16:09:14 +0100

    configuring circleCI

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       '@typescript-eslint/no-var-requires': 0,
+      "@typescript-eslint/ban-ts-ignore": "off",
   }
 }
