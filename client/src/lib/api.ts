c23ae2a742f57bcab42be74c821e3821300c7db6
Drew Jocham <drew.jocham@gmail.com>
2022-12-10 14:26:09 +0100

    updated config.yml and adjusted api.ts

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,22 +2,21 @@ import axios, {AxiosInstance} from "axios";
 import {TestResponse} from "@/proto/api_pb";
 import {EnvironmentHelper} from "@/lib/EnvironmentHelper";
 
+const url = new EnvironmentHelper()
+
 const grpcClient: AxiosInstance = axios.create({
+    baseURL: url.baseUrl,
     headers: {
         'content-type': 'application/json',
     },
     //params: {base64_encoded: 'true', fields: 'stdout'},
 });
 
-const url = new EnvironmentHelper()
-
-axios.defaults.url = url.baseUrl
-
 export const api = {
 
     async getTest() {
         try{
-            return await grpcClient.get<TestResponse>(url.baseUrl + "/v1/test")
+            return await grpcClient.get<TestResponse>( "/v1/test")
                 .then(res => {
                     console.log(url.baseUrl)
                     return res.data
